40480f3076023da10f633a090d08557708ede044
Sam <yzlindevelop@163.com>
2016-07-11 18:10:58 +0800

    添加TabBar图标, 背景图片; 添加WCNavigationController; Message界面初步搭建;

---
+++ Demo_Chat/Classes/Main/Controllers/WCNavigationController.m
@@ -0,0 +1,30 @@
+//
+//  WCNavigationController.m
+//  Demo_Chat
+//
+//  Created by admin on 16/7/11.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import "WCNavigationController.h"
+
+@interface WCNavigationController ()
+
+@end
+
+@implementation WCNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self.tabBarItem setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor colorWithRed:0 green:(189/255.f) blue:0 alpha:1]} forState:UIControlStateSelected];
+    
+    self.navigationBar.barTintColor = [UIColor blackColor];
+    self.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName:[UIColor whiteColor]};
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+@end
